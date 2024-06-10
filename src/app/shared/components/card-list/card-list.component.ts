37bba4548cf8f1f8122598e82217d7c4d841cf3a
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-06-10 20:59:07 -0300

    feat: Card list component

---
+++ src/app/shared/components/card-list/card-list.component.ts
@@ -0,0 +1,69 @@
+import { AsyncPipe, CommonModule, CurrencyPipe } from "@angular/common";
+import { ChangeDetectionStrategy, Component, inject, input } from "@angular/core";
+import { Product } from "../../../core/interfaces/product.interface";
+import { ButtonModule } from "primeng/button";
+import { ImgPathService } from "../../../core/services/img-path.service";
+import { SkeletonModule } from "primeng/skeleton";
+import { Router } from "@angular/router";
+import { ImgProductComponent } from "../img-product/img-product.component";
+
+@Component({
+  selector: "app-card-list",
+  standalone: true,
+  imports: [CommonModule, CurrencyPipe, AsyncPipe, ButtonModule, SkeletonModule, ImgProductComponent],
+  template: ` <div class="flex gap-3 bg-black border-1 border-round surface-border min-h-full px-3 py-4">
+    @if (loading()) {
+      <div class="w-3">
+        <p-skeleton styleClass="w-full h-10rem" />
+      </div>
+      <div class="flex flex-column justify-content-between w-9">
+        <p-skeleton styleClass="w-9" />
+        <p-skeleton styleClass="w-4" />
+        <div>
+          <p-skeleton styleClass="w-full" />
+          <p-button styleClass="w-full h-2rem mt-2 p-2 border-round-sm" severity="warning" [disabled]="true" />
+        </div>
+      </div>
+    } @else {
+      <div class="w-3">
+        <div class=" bg-white border-round  overflow-hidden cursor-pointer">
+          <app-img-product loading="lazy" [data]="data()" [padding]="3" />
+        </div>
+      </div>
+      <div class="flex flex-column justify-content-between w-9">
+        @if (data().title) {
+          <h3 class="text-lg m-0">{{ data().title }}</h3>
+        }
+        <div class="flex flex-column gap-4 w-full">
+          @if (data().price_special) {
+            <span class="text-lg font-medium text-blue-400 ml-2">
+              {{ data().price_special | currency: "ARS" : "symbol" : "1.2-2" : "" }}
+            </span>
+          }
+          <p-button
+            styleClass="w-full p-2 border-round-sm"
+            severity="warning"
+            [disabled]="data().stock <= 0"
+            (onClick)="navigateToProduct(data().url_search)"
+          >
+            <span class="text-base font-bold text-center w-full">
+              {{ data().stock <= 0 ? "Agotado" : "Comprar" }}
+            </span>
+          </p-button>
+        </div>
+      </div>
+    }
+  </div>`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CardListComponent {
+  private _router = inject(Router);
+  public _imgPathService = inject(ImgPathService);
+  public data = input.required<Product>();
+  public loading = input.required<boolean>();
+  public imgHover: boolean = false;
+
+  public navigateToProduct(url: string): void {
+    this._router.navigateByUrl(`/producto/${url}`);
+  }
+}
