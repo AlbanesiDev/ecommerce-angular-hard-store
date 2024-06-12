eee15f703fcbc0c362733c8cc1c27317b6e6abd5
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-06-12 14:10:51 -0300

    update: Home component

--- src/app/features/home/pages/home.component.ts
+++ src/app/features/home/pages/home.component.ts
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AsyncPipe, CommonModule } from "@angular/common";
-import { ChangeDetectionStrategy, Component, OnInit, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, DestroyRef, HostListener, OnInit, inject, signal } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 import { Store } from "@ngrx/store";
+import { combineLatest } from "rxjs";
 
 import { CarouselModule, CarouselResponsiveOptions } from "primeng/carousel";
 import { ProgressSpinnerModule } from "primeng/progressspinner";
@@ -19,68 +20,85 @@ import {
   selectProductsLoading,
 } from "../store/home.selectors";
 import * as HomeActions from "../store/home.actions";
+import * as HomeSelectors from "../store/home.selectors";
 @Component({
   selector: "app-home",
   standalone: true,
   imports: [CommonModule, AsyncPipe, CarouselModule, SkeletonModule, ProgressSpinnerModule, CardGridComponent],
   template: `
     <div class="flex flex-column justify-content-center gap-4">
-      @defer (on immediate) {
-        <p-carousel
-          styleClass="w-full"
-          ngSkipHydration
-          [value]="bannersSig()"
-          [numVisible]="1"
-          [numScroll]="1"
-          [circular]="true"
-          [draggable]="true"
-          [autoplayInterval]="4000"
-        >
-          <ng-template let-slide pTemplate="item">
-            <div class="h-full mx-2 px-3 py-4 border-1 bg-black surface-border border-round">
-              <img class="w-full min-h-full border-round" [src]="slide.src" [alt]="slide.alt" />
-            </div>
-          </ng-template>
-        </p-carousel>
-        @for (item of cardsSig(); track $index) {
-          <h2 class="capitalize text-xl">{{ item.title }}</h2>
-          <p-carousel
-            ngSkipHydration
-            [value]="item.products"
-            [numScroll]="1"
-            [numVisible]="4"
-            [circular]="true"
-            [draggable]="true"
-            [autoplayInterval]="item.interval"
-            [responsiveOptions]="responsiveOptions"
-          >
-            <ng-template let-product pTemplate="item">
-              <div class="h-full mx-2">
-                <app-card-grid [data]="product" [loading]="loadingProducts()" />
-              </div>
-            </ng-template>
-          </p-carousel>
+      <p-carousel
+        styleClass="w-full min-h-full"
+        ngSkipHydration
+        [value]="bannersSig()"
+        [numVisible]="1"
+        [numScroll]="1"
+        [circular]="true"
+        [draggable]="true"
+        [showNavigators]="showNavigators()"
+        [autoplayInterval]="4000"
+      >
+        <ng-template let-slide pTemplate="item">
+          <div class="h-full mx-2 border-1 bg-black surface-border border-round">
+            @if (loadingBanners()) {
+              <p-skeleton styleClass="w-full h-17rem" />
+            } @else if (bannerError()) {
+              <h3 class="text-center text-red-500">{{ bannerError() }}</h3>
+            } @else {
+              <img class="w-full min-h-full border-round-xs px-2 py-2" [src]="slide.src" [alt]="slide.alt" />
+            }
+          </div>
+        </ng-template>
+      </p-carousel>
+      @defer {
+        @if (productsError()) {
+          <h3 class="text-center text-red-500">{{ productsError() }}</h3>
+        } @else {
+          @for (item of productsSig(); track item.id) {
+            <h2 class="capitalize text-xl">{{ item.title }}</h2>
+            <p-carousel
+              ngSkipHydration
+              [value]="item.products"
+              [numScroll]="1"
+              [circular]="true"
+              [draggable]="true"
+              [showNavigators]="showNavigators()"
+              [autoplayInterval]="item.interval"
+              [responsiveOptions]="responsiveOptions"
+            >
+              <ng-template let-product pTemplate="item">
+                <div class="h-full mx-2">
+                  <app-card-grid [data]="product" [loading]="loadingProducts()" />
+                </div>
+              </ng-template>
+            </p-carousel>
+          }
         }
-      } @placeholder {
-        <div class="flex justify-content-center align-items-center min-h-calc">
-          <p-progressSpinner ariaLabel="loading" fill="var(--bg-orange-500)" />
-        </div>
       }
     </div>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export default class HomeComponent implements OnInit {
-  public bannersSig = signal<HomeBanners[]>([]);
-  public cardsSig = signal<HomeProducts[]>([]);
+  private _destroyRef = inject(DestroyRef);
+
+  public bannersSig = signal<HomeBanners[] | any[]>([]);
+  public productsSig = signal<HomeProducts[] | any[]>([]);
 
   public loadingBanners = signal<boolean>(false);
   public loadingProducts = signal<boolean>(false);
 
-  public bannerErrorMessage = signal<unknown>(undefined);
-  public productsErrorMessage = signal<unknown>(undefined);
+  public bannerError = signal<unknown>(undefined);
+  public productsError = signal<unknown>(undefined);
+
+  public showNavigators = signal<boolean>(true);
 
   public responsiveOptions: Array<CarouselResponsiveOptions> = [
+    {
+      breakpoint: "2560px",
+      numVisible: 5,
+      numScroll: 1,
+    },
     {
       breakpoint: "1024px",
       numVisible: 4,
@@ -96,20 +114,52 @@ export default class HomeComponent implements OnInit {
       numVisible: 2,
       numScroll: 1,
     },
+    {
+      breakpoint: "368px",
+      numVisible: 1,
+      numScroll: 1,
+    },
   ];
 
   constructor(private store: Store<{ home: HomeState }>) {
-    this.subscribeToStore(selectBanners, this.bannersSig);
-    this.subscribeToStore(selectProducts, this.cardsSig);
-    this.subscribeToStore(selectBannersLoading, this.loadingBanners);
-    this.subscribeToStore(selectProductsLoading, this.loadingProducts);
-    this.subscribeToStore(selectBannersFailure, this.bannerErrorMessage);
-    this.subscribeToStore(selectProductsFailure, this.productsErrorMessage);
+    this.updateResponsive();
+    this.subscribeToSelector(selectBanners, this.bannersSig);
+    this.subscribeToSelector(selectProducts, this.productsSig);
+    this.subscribeToSelector(selectBannersLoading, this.loadingBanners);
+    this.subscribeToSelector(selectProductsLoading, this.loadingProducts);
+    this.subscribeToSelector(selectBannersFailure, this.bannerError);
+    this.subscribeToSelector(selectProductsFailure, this.productsError);
   }
 
   ngOnInit(): void {
-    this.store.dispatch(HomeActions.loadBanners());
-    this.store.dispatch(HomeActions.loadProducts());
+    combineLatest([this.store.select(HomeSelectors.selectBanners), this.store.select(HomeSelectors.selectProducts)])
+      .pipe(takeUntilDestroyed(this._destroyRef))
+      .subscribe(([banners, products]) => {
+        if (!banners || banners.length === 0) {
+          this.store.dispatch(HomeActions.loadBanners());
+        }
+        if (!products || products.length === 0) {
+          this.store.dispatch(HomeActions.loadProducts());
+        }
+      });
+    if (this.loadingBanners()) {
+      this.bannersSig.set([{ src: "", alt: "" }]);
+    }
+    if (this.loadingProducts()) {
+      this.productsSig.set([
+        { id: 0, title: "productos destacados", products: [{}, {}, {}, {}, {}], interval: 4000 },
+        { id: 1, title: "ultimos ingresados", products: [{}, {}, {}, {}, {}], interval: 4000 },
+      ]);
+    }
+  }
+
+  @HostListener("window:resize", ["$event"])
+  private updateResponsive() {
+    if (window.innerWidth > 768) {
+      this.showNavigators.set(true);
+    } else {
+      this.showNavigators.set(false);
+    }
   }
 
   /**
@@ -117,7 +167,7 @@ export default class HomeComponent implements OnInit {
    * @param selector - The selector to subscribe to.
    * @param signal - The signal to set the value to.
    */
-  private subscribeToStore<T>(selector: any, signal: any): void {
+  private subscribeToSelector<T>(selector: any, signal: any): void {
     this.store
       .select(selector)
       .pipe(takeUntilDestroyed())
