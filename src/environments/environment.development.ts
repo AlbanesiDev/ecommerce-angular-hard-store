5cf584ef3554124444488b10e0df3dc226a924a0
Joaquin Albanesi <114169841+AlbanesiDev@users.noreply.github.com>
2024-06-13 15:28:12 -0300

    add: Environments

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,22 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    projectId: "***************",
+    appId: "***************",
+    storageBucket: "***************",
+    apiKey: "***************",
+    authDomain: "***************",
+    messagingSenderId: "***************",
+    measurementId: "***************",
+  },
+  img_cdn: "https://***************.***************",
+  mock: {
+    homeBanner: "/assets/mocks/home-banners.json",
+    homeProducts: "/assets/mocks/home-products.json",
+    shopProducts: "/assets/mocks/shop-products.json",
+  },
+  endpoint: {
+    homeBanners: "banners",
+    homeProducts: "products",
+  },
+};
